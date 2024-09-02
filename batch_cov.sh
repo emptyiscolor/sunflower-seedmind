ba53b9fd214b1fa647c83e8bb93a1f66180a10cc
emptyiscolor <emptyiscolor@gmail.com>
2024-09-02 09:42:11 -0600

    update batch scripts and data

--- batch_cov.sh
+++ batch_cov.sh
@@ -15,17 +15,41 @@ function pre_build_project() {
 
 function count_seedgen_cov() {
   pushd $OSSFUZZ_DIR
-  find build/cov_report/seedgen -name summary.json | grep report_target | while read -r summary_path; do
-      binary_name=$(basename "$(dirname "$(dirname "$summary_path")")")
-      project=$(basename "$(dirname "$(dirname "$(dirname "$(dirname "$summary_path")")")")")
+
+  echo > "$COV_CSV_FILE"
+
+  while IFS= read -r line; do
+    # Split the line into fields using ':' as the delimiter
+    IFS=',' read -r -a fields <<< "$line"
+
+    # Extract project_name and source_code_file
+    project_name="${fields[0]}"
+    binary_name="${fields[1]}"
+    source_code_file_with_info="${fields[2]}"
+
+    # Remove the line number and column number from source_code_file
+    source_code_file="${source_code_file_with_info%:*:*}"
+
+    # echo "Project Name: $project_name, Binary Name: $binary_name, Source Code File: $source_code_file"
+    summary_path="$OSSFUZZ_DIR/build/cov_report/seedgen/$project_name/report_target/$binary_name/linux/summary.json"
+
+    if [ -d "$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" ] && [ -f "$OSSFUZZ_DIR/build/corpus/seedgen/$project_name/$binary_name.json" ] && [ -f "$summary_path" ] ; then
+      # echo "Geting code coverage: $project_name, $binary_name"
       cov=$(jq .data[].totals.lines.percent < "$summary_path")
-      printf "$project\t$binary_name\t$cov\n" | tee -a $COV_CSV_FILE
-  done
+    else
+      # echo "No coverage report found for $project_name, $binary_name"
+      cov="NA"
+    fi
+
+    echo "$project_name,$binary_name,$cov" | tee -a $COV_CSV_FILE
+
+  done < "$CSV_HARNESS_FILE"
 
   popd
 }
 
 function generate_cov() {
+  pushd $OSSFUZZ_DIR
   # python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir=$corpus_dir $project --no-serve
   while IFS= read -r line; do
     # Split the line into fields using ':' as the delimiter
@@ -41,10 +65,11 @@ function generate_cov() {
 
     echo "Project Name: $project_name, Binary Name: $binary_name, Source Code File: $source_code_file"
 
-    if [ -d "$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" ] ; then
+    if [ -d "$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" ] && [ -f "$OSSFUZZ_DIR/build/corpus/seedgen/$project_name/$binary_name.json" ] ; then
       echo "Generating code coverage: $project_name"
-    #   python infra/helper.py build_fuzzers --sanitizer=coverage $project_name
-      timeout 20m python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir="$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" --no-serve $project_name 
+      #   python infra/helper.py build_fuzzers --sanitizer=coverage $project_name
+      echo "python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir="$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" --no-serve $project_name "
+      timeout 30m python infra/helper.py coverage --fuzz-target=$binary_name --corpus-dir="$OSSFUZZ_DIR/build/corpus/seedgen/$project_name" --no-serve $project_name 
       if [ $? -eq 124 ]; then
         echo "Timeout reached. Running another command..."
         docker stop $(docker ps -q)
@@ -54,10 +79,11 @@ function generate_cov() {
     fi
 
   done < "$CSV_HARNESS_FILE"
+  popd
 }
 
 # 1. build targets
-pre_build_project
+# pre_build_project
 # 2. collect coverage
 generate_cov
 # 3. save the output to a csv
--- batch_run.sh
+++ batch_run.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-TO_GENERATED_FILE="./data/input.csv"
-TIMEOUT=30m
+TO_GENERATED_FILE="./data/input_sample_4.csv"
+TIMEOUT=30
+
+function clean_old_containers() {
+    # Stop Docker containers running for more than 4 hours
+    echo "Stopping Docker containers running for more than 4 hours..."
+    containers=$(docker ps --format '{{.ID}} {{.RunningFor}}' | grep -E '([0-9]+) (hours|days|weeks|months|years)' | while read -r id time; do
+        hours=$(echo $time | grep -oE '^[0-9]+')
+        unit=$(echo $time | grep -oE '(hours|days|weeks|months|years)')
+        if [ "$unit" == "hours" ] && [ "$hours" -gt 3 ]; then
+            echo $id
+        elif [ "$unit" != "hours" ]; then
+            echo $id
+        fi
+    done)
+
+    for container in $containers; do
+        docker stop $container
+    done
+
+    # Delete folders under ./tmp older than 4 hours (depth 1)
+    echo "Deleting folders under ./tmp older than 4 hours..."
+    find ./.tmp -mindepth 1 -maxdepth 1 -type d -mmin +240 -exec rm -rf {} \;
+
+    echo "Cleanup completed."
+}
+
 
 while IFS= read -r harness; do
     IFS=',' read -r -a fields <<< "$harness"
@@ -12,30 +37,36 @@ while IFS= read -r harness; do
     echo $fields
     mkdir -p /tmp/logs/$project_name
     # timeout $TIMEOUT echo python oss-fuzz.py $project_name $binary_name 2>&1 
-    timeout $TIMEOUT python oss-fuzz.py --level 5 --budget 0.5 $project_name $binary_name 2>&1 | tee /tmp/logs/$project_name/batch_run.log
+    time_needed=$(echo $(( $TIMEOUT * $(echo $binary_name | wc -w))))
+    echo "Recaculate the timeout: $time_needed"
+    echo "RUN: python oss-fuzz.py --level 5 --budget 0.5 $project_name $binary_name"
+    # for binary in ${binary_name}; do
+    # timeout "${TIMEOUT}m"  python oss-fuzz.py --level 5 --budget 0.5 $project_name $binary >/tmp/logs/$project_name/batch_run.log 2>&1 &
+    python oss-fuzz.py --level 5 --budget 0.5 $project_name $binary_name >/tmp/logs/$project_name/batch_run.log 2>&1 &
+    # done
     # Capture the exit code of the `timeout` command
     exit_code=$?
 
-    if [ $? -eq 124 ]; then
-        echo "Timeout reached. Running another command..."
-        docker stop $(docker ps -q)
-    elif [ $exit_code -eq 137 ]; then
-        echo "Process was killed. exiting..."
-        docker stop $(docker ps -q)
-    elif [ $exit_code -ne 0 ]; then
+    # if [ $? -eq 124 ]; then
+    #     echo "Timeout reached. Running another command..."
+    #     docker stop $(docker ps -q)
+    # elif [ $exit_code -eq 137 ]; then
+    #     echo "Process was killed. exiting..."
+    #     docker stop $(docker ps -q)
+    if [ $exit_code -ne 0 ]; then
         echo "Python script exited unexpectedly with code $exit_code."
         echo "$project_name $binary_name" >> /tmp/seed_genexceptions.txt
+        docker stop $(docker ps -q)
+        exit 1
     else
-        echo "Seedgen for $project_name completed."
+        echo "Seedgen for $project_name , $binary_name started."
     fi
 
-    sudo rm -rf .tmp/*
+    sleep 300
+
+    # sudo rm -rf .tmp/*
+
     # remove large seeds
     find  oss-fuzz/build/corpus/seedgen -type f -size +2M -delete
-#     token_used=$(cat /tmp/logs/$project_name/batch_run.log | grep 'Tokens Used' | cut -d':' -f2)
-#     prompt_tokens=$(cat /tmp/logs/$project_name/batch_run.log | grep 'Prompt Tokens' | cut -d':' -f2)
-#     complete_tokens=$(cat /tmp/logs/$project_name/batch_run.log | grep 'Completion Tokens' | cut -d':' -f2)
-#     total_cost=$(cat /tmp/logs/$project_name/batch_run.log | grep 'Total Cost' | cut -d':' -f2 | tr -d '$')
-#     find /tmp -depth -maxdepth 1 -type d | grep -E '[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}' | grep -v devcontain | xargs -I {} sudo rm -rf {} 
-#     echo "$project_name,$binary_name,$token_used,$prompt_tokens,$complete_tokens,$total_cost" >> /tmp/seedgen_metrics.csv
+
 done < "$TO_GENERATED_FILE"
