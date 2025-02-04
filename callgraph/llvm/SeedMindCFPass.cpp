45e94bd0c8fa8d9b70430fbbbaaf464a32f61b73
Wenxuan Shi <whexy@outlook.com>
2025-02-04 21:29:16 +0000

    refactor: enhance call graph tracking with direct function pointer injection
    This commit updates the SeedMindCFPass and runtime library to:
    - Inject call graph tracking at each function call site instead of function entry
    - Use direct function pointers for caller and callee tracking
    - Improve library function filtering
    - Simplify symbolization logic in the runtime

--- callgraph/llvm/SeedMindCFPass.cpp
+++ callgraph/llvm/SeedMindCFPass.cpp
@@ -1,65 +1,93 @@
 // Wenxuan Shi <wenxuan.shi@northwestern.edu>
 // 2024, Northwestern University. All rights reserved.
 
-// this is a function pass that will call the `__cyg_profile_func_enter_fine_i_will_do_it_myself` function at the beginning of each function
-
+#include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include <llvm/IR/PassManager.h>
 #include <llvm/Passes/OptimizationLevel.h>
 #include <llvm/Support/Compiler.h>
-#include "llvm/IR/DebugInfoMetadata.h"
 
 using namespace llvm;
 
+bool is_usr_library(const Function *func) {
+  if (llvm::DISubprogram *SP = func->getSubprogram()) {
+    std::string Filename = SP->getFilename().str();
+    std::string Directory = SP->getDirectory().str();
+    std::string FullPath =
+        Directory.empty() ? Filename : Directory + "/" + Filename;
+    return FullPath.compare(0, 4, "/usr") == 0;
+  }
+  return true;
+}
+
 class SeedMindCFPass : public PassInfoMixin<SeedMindCFPass> {
 public:
-    static char ID;
-    explicit SeedMindCFPass() { }
-    static bool isRequired() { return true; }
+  static char ID;
+  explicit SeedMindCFPass() {}
+  static bool isRequired() { return true; }
 
-    PreservedAnalyses run(Module& M, ModuleAnalysisManager& AM)
-    {
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
 
-        FunctionCallee hookFunc = M.getOrInsertFunction(
-            "__seedmind_func_enter",
-            FunctionType::get(Type::getVoidTy(M.getContext()), false));
+    // we don't care about the function type of caller and callee, we just
+    // need the function pointer, so we can just use void * as the function
+    // pointer type
+    FunctionCallee hookFunc = M.getOrInsertFunction(
+        "__seedmind_record_func_call",
+        FunctionType::get(
+            Type::getVoidTy(M.getContext()),
+            {PointerType::get(
+                 FunctionType::get(Type::getVoidTy(M.getContext()), {}, false),
+                 0),
+             PointerType::get(
+                 FunctionType::get(Type::getVoidTy(M.getContext()), {}, false),
+                 0)},
+            false));
 
-        for (auto& F : M.functions()) {
-            // Use Debug Info Metadata to filter out functions that come from libraries under /usr
-            if (llvm::DISubprogram *SP = F.getSubprogram()) {
-                std::string Filename = SP->getFilename().str();
-                std::string Directory = SP->getDirectory().str();
+    for (auto &F : M.functions()) {
+      if (is_usr_library(&F)) {
+        continue;
+      }
 
-                std::string FullPath = Directory.empty() ? Filename : Directory + "/" + Filename;
-
-                if (FullPath.compare(0, 4, "/usr") != std::string::npos) {
-                    continue;
-                }
-            }
-
-            if (F.isDeclaration() || F.empty())
+      for (auto &BB : F) {
+        for (auto &I : BB) {
+          if (auto *callBase = dyn_cast<CallBase>(&I)) {
+            Value *callee = nullptr;
+            if (callBase->isIndirectCall()) {
+              callee = callBase->getCalledOperand();
+            } else {
+              Function *calleeFunc = callBase->getCalledFunction();
+              if (is_usr_library(calleeFunc)) {
                 continue;
-            IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
-            Builder.CreateCall(hookFunc);
-
+              }
+              if (calleeFunc->isIntrinsic()) {
+                continue;
+              }
+              callee = calleeFunc;
+            }
+            Function *caller = callBase->getCaller();
+            // inject the call to the hook function, with caller and callee
+            IRBuilder<> Builder(callBase);
+            Builder.CreateCall(hookFunc, {caller, callee});
+          }
         }
+      }
+    }
 
-        return PreservedAnalyses::all();
-    };
+    return PreservedAnalyses::all();
+  };
 };
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
-llvmGetPassPluginInfo()
-{
-    return {
-        LLVM_PLUGIN_API_VERSION, "SeedMindCFPass", LLVM_VERSION_STRING,
-        [](PassBuilder& PB) {
-            PB.registerOptimizerLastEPCallback([](ModulePassManager& MPM, OptimizationLevel Level) {
-                MPM.addPass(SeedMindCFPass());
-            });
-        }
-    };
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "SeedMindCFPass", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerOptimizerLastEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                  MPM.addPass(SeedMindCFPass());
+                });
+          }};
 }
