1de10ddb76c37a724be62ba9e6c65d8c4da55ef9
emptyiscolor <emptyiscolor@gmail.com>
2025-05-20 12:50:16 +0000

    update supported enum type to replace seedmini

--- infra/aixcc.py
+++ infra/aixcc.py
@@ -519,7 +519,7 @@ def run_mcp_mode(
                 task,
                 harness_binary,
                 os.path.join(fuzzer_dir, "seeds"),
-                "seedmcp",
+                "seedmini",
                 gen_model
             )
             print(
